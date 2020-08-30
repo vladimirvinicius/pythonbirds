ba82a5937468782fa6d2be2bc1311e2db798dbd1
vladimirvinicius <vladimirvinicius@gmail.com>
2020-08-30 01:02:07 -0300

    Implementada composição e finalização de Doctests

--- oo/carro.py
+++ oo/carro.py
@@ -90,7 +90,7 @@ O     L
     'Leste'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    'Norte'
+    'Sul'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
     'Oeste'
@@ -145,9 +145,25 @@ class Direcao:
         #else:
         #    self.valor = NORTE
 
-class Carro():
-    pass
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
 
+    def calcular_velocidade(self):
+        return self.motor.velocidade
 
+    def acelerar(self):
+        self.motor.acelerar()
 
+    def frear(self):
+        self.motor.frear()
 
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_direita()
